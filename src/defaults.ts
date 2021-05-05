2441aa429e5c53b7796df3ffb86c5f6457b14d69
tckerr <tckerr@gmail.com>
2021-05-05 08:43:46 -0400

    fixing build issue

--- src/defaults.ts
+++ src/defaults.ts
@@ -10,6 +10,7 @@ export function buildDefaultConfig<T>() : Config<T> {
         rootObjectCallbacks: true,
         runCallbacks: true,
         graphMode: 'finiteTree',
+        parallelizeAsyncCallbacks: false,
         callbacks: []
     }
 }
