7256c300ff837d8f0f0440e04dd4cdaf56375318
tckerr <tckerr@gmail.com>
2023-08-05 09:27:37 -0400

    Updating scripts

--- finalize-and-publish.sh
+++ finalize-and-publish.sh
@@ -1,7 +1,8 @@
-# update docs
-# create git tag in format vM.m.p
-
 set -e
-npm version $1
-npm publish
-git push --tags
+
+echo "Please input a version number"
+read v
+
+npm version "v$v"
+#npm publish
+#git push --tags
